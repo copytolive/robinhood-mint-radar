42f46130f6fe75d962469bd3513e2487077ecfd5
copytolive <copytolive@gmail.com>
2026-08-28 17:06:08 +0700

    test(relevance): reject live liquidity-book position token

--- tests/test_relevance.py
+++ tests/test_relevance.py
@@ -10,6 +10,9 @@ class RelevanceTests(unittest.TestCase):
             with self.subTest(name=name):
                 r=evaluate_relevance('ERC721',name,'contract GenericERC721 { function tokenURI() external {} }')
                 self.assertEqual(r['state'],'REJECT')
+    def test_liquidity_book_token_rejected(self):
+        r=evaluate_relevance('ERC1155','Liquidity Book Token','contract LBPair { function uri(uint256) external view returns (string memory) {} }')
+        self.assertEqual(r['state'],'REJECT')
     def test_seadrop_collectible_passes(self):
         r=evaluate_relevance('ERC721','ERC721SeaDropCloneable','contract ERC721SeaDropCloneable { function tokenURI() external {} }')
         self.assertEqual(r['state'],'PASS')
