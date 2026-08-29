7ec6a57ff977dd7a7caf2cfcbd8e2b0716109381
copytolive <copytolive@gmail.com>
2026-08-29 21:39:37 +0700

    fix(runtime): use bounded moving-tip scanner

--- radar/cli.py
+++ radar/cli.py
@@ -4,7 +4,7 @@ import sys
 import time
 from . import config
 from .notify import notify_qualified
-from .fast_live_scanner import FastLiveRadarScanner as RadarScanner
+from .bounded_fast_live_scanner import BoundedFastLiveRadarScanner as RadarScanner
 from .scanner import write_status
 
 
